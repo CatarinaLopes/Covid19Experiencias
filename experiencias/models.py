ae5206fb9938c4104faed9c860c86afbd5cfe10d
RinaFox <62477985+CatarinaLopes@users.noreply.github.com>
2020-05-03 12:02:18 +0100

    Update models.py

--- experiencias/models.py
+++ experiencias/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 
 class UserPost(models.Model):
-    text = models.CharField(max_length=500)
+    text = models.CharField(min_length=10,max_length=500)
     img = models.CharField(max_length=500)
-    title = models.CharField(max_length=100)
+    title = models.CharField(min_lenght=5,max_length=100)
     pub_data = models.DateTimeField('publishing date')
     user_name = models.CharField(max_length=25, default='')
     score = models.IntegerField(default=0)
